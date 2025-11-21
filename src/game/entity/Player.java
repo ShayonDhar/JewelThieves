9d3e72153fe0834f09028acec369aadd5b11739b
Alex Samuel <alexanderdsamuel@gmail.com>
2025-11-21 12:29:03 +0000

    Removed entity id for now as it was causing problems

--- src/game/entity/Player.java
+++ src/game/entity/Player.java
@@ -25,15 +25,14 @@ public class Player extends Entity {
     /**
      * Constructs a new Player entity.
      *
-     * @param entityID       unique ID of the player
      * @param y              y coordinate of the player
      * @param x              x coordinate of the player
      * @param direction      direction the player is facing
      * @param alive          the alive state of the player
      * @param blocksMovement whether the player blocks movement of other entities
      */
-    public Player(int entityID, int y, int x, Direction direction, boolean alive, boolean blocksMovement) {
-        super(entityID, ENTITY_NAME, y, x, direction, alive, blocksMovement);
+    public Player(int y, int x, Direction direction, boolean alive, boolean blocksMovement) {
+        super(ENTITY_NAME, y, x, direction, alive, blocksMovement);
     }
 
     /**
