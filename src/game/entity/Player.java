fefc9e9e80978397ee816a4a9ece866b39e11aaf
Alex Samuel <alexanderdsamuel@gmail.com>
2025-11-21 15:43:23 +0000

    ID removed from parameter

--- src/game/entity/Player.java
+++ src/game/entity/Player.java
@@ -31,7 +31,7 @@ public class Player extends Entity {
      * @param alive          the alive state of the player
      * @param blocksMovement whether the player blocks movement of other entities
      */
-    public Player(int entityID, int y, int x, Direction direction, boolean alive, boolean blocksMovement) {
+    public Player(int y, int x, Direction direction, boolean alive, boolean blocksMovement) {
         super(ENTITY_NAME, y, x, direction, alive, blocksMovement);
     }
 
