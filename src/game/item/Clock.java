a3e4bcc226d8b469570098d53802bfc7c4f4fd49
Elijah <eogunjebe@gmail.com>
2025-11-23 15:48:55 +0000

    Added a method to add or subtract time remaining for the player to complete the level based on whether it was the player or a thief who collected the clock. Also added getters and setters for the value of the added time.

--- src/game/item/Clock.java
+++ src/game/item/Clock.java
@@ -1,8 +1,14 @@
 package game.item;
 
+import game.entity.Entity;
+import game.entity.EntityName;
+import game.level.Level;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Clock extends Item{
+    private Level level;
+    private int addedTime;
+
     /**
      * Constructor that all the items will use.
      * It has all the properties they have in common
@@ -17,10 +23,49 @@ public class Clock extends Item{
      */
     public Clock(String itemName, int itemID, int x, int y, boolean isOn) {
         super(itemName, itemID, x, y, isOn);
+        this.level = level;
+        this.addedTime = addedTime;
     }
 
     @Override
     public void draw(GraphicsContext gc) {
 
+    }
+
+    /**
+     * If the player collects a clock it will add the additional time provided
+     * by the clock to the time remaining for the player to complete the level.
+     * If a smart thief or floor following thief collects a clock it will
+     * deduct the same value as the additional time provided by the clock to
+     * the time remaining to complete the level
+     * @param entityName the name of the entity, e.g. PLAYER, SMART_THIEF
+     */
+    @Override
+    public void collectItem(Entity entityName) {
+        if (!isOn) {
+            return;
+        }
+
+        if (entityName.getX() == x && entityName.getY() == y) {
+            if (entityName.getEntityName().equals("PLAYER")) {
+                level.updateLevel(addedTime);
+            }
+
+
+            if (entityName.getEntityName().equals("SMART_THIEF") || entityName.getEntityName().equals("FLOOR_FOLLOWING_THIEF")) {
+                level.updateLevel(addedTime * -1);
+            }
+
+        }
+
+        isOn = false;
+    }
+
+    public void setAddedTime(int addedTime) {
+        this.addedTime = addedTime;
+    }
+
+    public int getAddedTime(int addedTime) {
+        return addedTime;
     }
 }
