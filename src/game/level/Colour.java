963154e8ea7bf5a5f893b08230714b7bbddb896b
Alex Samuel <alexanderdsamuel@gmail.com>
2025-11-24 19:44:17 +0000

    Made some colour enum adjustments.

--- src/game/level/Colour.java
+++ src/game/level/Colour.java
@@ -1,11 +1,35 @@
 package game.level;
 
-public enum Colour {
-    RED,
-    GREEN,
-    BLUE,
-    YELLOW,
-    CYAN,
-    MAGENTA
-}
+import javafx.scene.paint.Color;
 
+public enum Colour {
+    RED('R', Color.RED),
+    GREEN('G', Color.GREEN),
+    BLUE('B', Color.BLUE),
+    YELLOW('Y', Color.YELLOW),
+    CYAN('C', Color.CYAN),
+    MAGENTA('M', Color.MAGENTA);
+
+    private final char code;
+    private final Color fxColor;
+
+    Colour(char code, Color fxColor) {
+        this.code = code;
+        this.fxColor = fxColor;
+    }
+
+    public char getCode() {
+        return code;
+    }
+
+    public Color getFxColor() {
+        return fxColor;
+    }
+
+    public static Colour fromCode(char c) {
+        for (Colour col : values()) {
+            if (col.code == c) return col;
+        }
+        throw new IllegalArgumentException("Invalid colour code: " + c);
+    }
+}
