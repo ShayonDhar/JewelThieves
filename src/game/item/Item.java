0fb97bded870628f156ce2ba983688b78a664894
Alex Samuel <alexanderdsamuel@gmail.com>
2025-11-26 11:49:08 +0000

    added javadoc comment for missing parameter

--- src/game/item/Item.java
+++ src/game/item/Item.java
@@ -31,7 +31,7 @@ public abstract class Item {
      * Constructor that all the items will use.
      * It has all the properties they have in common
      * But each item will have different String, int or boolean for each attribute
-     * @param itemName is the name of the item, e.g. Gate
+     * @param itemName name of the item for example gate.
      * @param itemID is the ID of the item, e.g. if there were 5 bombs, one ID would be 4
      * @param x is the x coordinate of the location of the item on the map
      * @param y is the y coordinate of the location of the item on the map
