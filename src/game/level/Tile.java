8a251d02d0eea623de3b205f687b935dc020998f
Rhys Dunn <zonkedcape@gmail.com>
2025-12-02 17:07:49 +0000

    Got rid of bomb field as it was being stored separately. Was unnecessary

--- src/game/level/Tile.java
+++ src/game/level/Tile.java
@@ -29,15 +29,11 @@ import javafx.scene.shape.StrokeType;
  * This class also manages bomb behaviour.
  *
  * @author Alex Samuel, Shayon Dhar
- * @version 1.0
  */
 
 public class Tile {
 
     private static final int MAX_COLOURS = 4;
-    private static final int TILE_INNER_SQUARE_SIZE = 25;
-    private static final int RECTANGLE_THREE_INDEX = 3;
-    private static final double OPACITY = 0.3;
 
     private final int x;
     private final int y;
@@ -51,13 +47,7 @@ public class Tile {
 
     private List<NPC> npc = new ArrayList<>();
 
-    /**
-     * Tile constructor.
-     *
-     * @param x c-coordinate of the tile
-     * @param y y-coordinate of the tile
-     * @param colours the list of tile colours
-     */
+    private static final int TILE_INNER_SQUARE_SIZE = 25;
 
     public Tile(int x, int y, Color[] colours) {
         validateColourSize(colours.length);
@@ -75,16 +65,9 @@ public class Tile {
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
-
-    /**
-     * Checks whether a tile has a gate.
-     *
-     * @return whether the tile has a gate
-     */
 
     public boolean hasGate() {
         return gate != null;
@@ -175,53 +158,53 @@ public class Tile {
     /**
      * Method to convert a tile into a StackPane JavaFX object.
      * This allows for a tile to be displayed within a StackPane on the TileGrid.
-     *
      * @return a StackPane object for the GameController to read
      */
     public StackPane toStackPane() {
+        StackPane root = new StackPane();
         GridPane gridPane = new GridPane();
 
         Rectangle r0 = createOutlinedRect(colours[0]);
         Rectangle r1 = createOutlinedRect(colours[1]);
         Rectangle r2 = createOutlinedRect(colours[2]);
-        Rectangle r3 = createOutlinedRect(colours[RECTANGLE_THREE_INDEX]);
+        Rectangle r3 = createOutlinedRect(colours[3]);
 
         gridPane.add(r0, 0, 0);
         gridPane.add(r1, 1, 0);
         gridPane.add(r2, 0, 1);
         gridPane.add(r3, 1, 1);
-        StackPane root = new StackPane();
+
         root.getChildren().add(gridPane);
         return root;
     }
 
     /**
      * Creates a rectangle with an outline stroke.
-     *
      * @param fill the fill color of the rectangle
      * @return a Rectangle with stroke applied
      */
     private Rectangle createOutlinedRect(Color fill) {
         Rectangle rect = new Rectangle(TILE_INNER_SQUARE_SIZE, TILE_INNER_SQUARE_SIZE, fill);
 
-        rect.setStroke(new Color(0, 0, 0, OPACITY));
+        rect.setStroke(new Color(0, 0, 0, 0.3));
         rect.setStrokeWidth(1);
         rect.setStrokeType(StrokeType.INSIDE);
 
         return rect;
     }
 
+
     @Override
     public String toString() {
-        return "Tile{"
-                + "x=" + x
-                + ", y=" + y
-                + ", colours=" + Arrays.toString(colours)
-                + ", item=" + item
-                + ", gate=" + gate
-                + ", bomb=" + bomb
-                + ", isExit=" + isExit
-                + ", npc=" + npc
-                + '}';
+        return "Tile{" +
+                "x=" + x +
+                ", y=" + y +
+                ", colours=" + Arrays.toString(colours) +
+                ", item=" + item +
+                ", gate=" + gate +
+                ", bomb=" + (item instanceof Bomb) +
+                ", isExit=" + isExit +
+                ", npc=" + npc +
+                '}';
     }
 }
