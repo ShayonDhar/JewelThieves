fdc3fedd3f386cd734557820d7e551895dfd7d27
Alex Samuel <alexanderdsamuel@gmail.com>
2025-12-02 12:03:54 +0000

    Added javadoc comments and removed all checkstyle errors

--- src/game/item/ItemType.java
+++ src/game/item/ItemType.java
@@ -1,10 +1,17 @@
 package game.item;
 
+/**
+ * Defines all the entity types.
+ *
+ * @author Anton W
+ * @version 1.0
+ */
+
 public enum ItemType {
     BOMB,
     CLOCK,
     DOOR,
     GATE,
     LEVER,
-    LOOT;
+    LOOT
 }
