77bddb9e4adc05bb88961cf3832855ab4295a011
Alex Samuel <alexanderdsamuel@gmail.com>
2025-12-04 09:37:43 +0000

    created player profile class

--- src/game/playerProfile/PlayerProfile.java
+++ src/game/playerProfile/PlayerProfile.java
@@ -1,4 +1,32 @@
-package playerProfile;
+package game.playerProfile;
+
+/**
+ * PlayerProfile which allows playerProfiles to be created.
+ *
+ * @author Alex Samuel
+ * @version 1.0
+ */
 
 public class PlayerProfile {
+    protected String name;
+    protected int maxLevelUnlocked;
+
+    /**
+     * Player profile constructor
+     * The player is automatically assigned with level 1 unlocked.
+     *
+     * @param name the name of the player
+     */
+    public PlayerProfile(String name) {
+        this.name = name;
+        this.maxLevelUnlocked = 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxLevelUnlocked() {
+        return maxLevelUnlocked;
+    }
 }
