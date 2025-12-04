b056d622ee17b3e14de9afa153b191103b0157b3
Alex Samuel <alexanderdsamuel@gmail.com>
2025-12-04 12:10:39 +0000

    Revert "Changed menu controller so when new game pressed it takes you to the PlayerProfile menu."
    This reverts commit 73fc514fffe2fdf52ebede6112f04c9a2f1df512.

--- src/game/MenuController.java
+++ src/game/MenuController.java
@@ -1,6 +1,5 @@
 package game;
 
-import game.playerProfile.ProfileController;
 import game.save.GameSaveManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -123,9 +122,9 @@ public class MenuController {
 
         try {
             // Load FXML using FXMLLoader instance (not static)
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("ProfileSelection.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("GameGraphics.fxml"));
             Pane root = loader.load();
-            ProfileController controller = loader.getController();
+            GameController controller = loader.getController();
 
             // New stage
             Stage gameStage = new Stage();
@@ -134,6 +133,8 @@ public class MenuController {
             Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
             root.setStyle("-fx-background-color: black");
 
+            // Register key input into the GameController
+            scene.setOnKeyPressed(controller::onKeyPressed);
 
             // Setting the scene and displaying it
             gameStage.setScene(scene);
