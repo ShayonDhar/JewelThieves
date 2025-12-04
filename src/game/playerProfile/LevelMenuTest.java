99c56481bf4f0865706b47df63ee29255ccf4278
Alex Samuel <alexanderdsamuel@gmail.com>
2025-12-04 10:24:48 +0000

    Created temporary menu test class to display the menu

--- src/game/playerProfile/LevelMenuTest.java
+++ src/game/playerProfile/LevelMenuTest.java
@@ -11,16 +11,16 @@ public class LevelMenuTest extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Load the FXML
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("levelmenu.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LevelMenu.fxml"));
         Parent root = loader.load();
 
-        // Create a scene
         Scene scene = new Scene(root, 800, 600);
 
-        // Apply CSS
-        scene.getStylesheets().add(getClass().getResource("levelmenu.css").toExternalForm());
+        // Linking the css file
+        scene.getStylesheets().add(
+                getClass().getResource("levelMenu.css").toExternalForm()
+        );
 
-        // Set up stage
         primaryStage.setTitle("Level Menu Test");
         primaryStage.setScene(scene);
         primaryStage.show();
